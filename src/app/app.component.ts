a93265e260a562035aa47b3d65be306852a994a2
shashwat <shashwatbhadoriya17@gmail.com>
2022-12-15 22:58:49 +0530

    Initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  val = '';
+
+  appendexp(event:any){
+
+    this.val += (event.currentTarget.value);
+    // console.log(event.currentTarget);
+  }
+
+  res(){
+    if(this.val === ''){
+      return ;
+    }
+    try {
+      // Evaluate the expression using the JavaScript eval() function
+      this.val = eval(this.val);
+    } catch (error) {
+      // If there is an error, show an error message
+      this.val = 'Error: Invalid expression';
+    }
+    this.val = this.val.toString();
+  }
+ 
+  backspace(){
+    if(this.val !== ''){
+    this.val = this.val.slice(0,this.val.length-1);
+    }
+  }
+
+  clear(){
+    this.val = "";
+  }
 }
